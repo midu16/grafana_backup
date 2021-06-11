2831896a29e7b50b4596b2ec07bf95d7ffca6f1f
midu16 <idumihai16@gmail.com>
2021-06-11 19:04:50 +0200

    updated

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ __author__ = 'Mihai IDU'
 import argparse
 import time
 import datetime
-from custom_classes import saveDashboards, saveDatasources
+#from custom_classes import saveDashboards, saveDatasources
+import os, errno
+
+def CreateNestedDirectors(path):
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
 
 if __name__ == '__main__':
     # defining the global variable to be accessible to the upper
@@ -13,9 +22,14 @@ if __name__ == '__main__':
     global compressed_dashboards_name
     global compressed_datasources_name
     global GRAFANA_TOKEN
+    global backup_path
     compressed_dashboards_name = "dashboards.tar.gz"
     compressed_datasources_name = "datasources.tar.gz"
     time = datetime.datetime.now()
     ISO_8601_time = time.isoformat()
     parser = argparse.ArgumentParser(description="Process design for Back-up the Grafana dashboards and datasources - Mihai IDU 2021")
     parser.add_argument("-p", "--path", type=str, help="Mention the path where the grafana_backup would manage the backups")
+    parser.add_argument("-k", "--api-key", type=str, help="")
+    args = parser.parse_args()
+    backup_path = args.path
+    GRAFANA_TOKEN = args.api_key

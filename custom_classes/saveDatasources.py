8222cba838713aed23fe257fa59a0583d1e2b312
midu16 <idumihai16@gmail.com>
2021-06-14 23:38:29 +0200

    improving the cli of grafana_backup

--- custom_classes/saveDatasources.py
+++ custom_classes/saveDatasources.py
@@ -12,13 +12,13 @@ def save_datasource(file_name, datasource_setting):
     file_path = folder_path + '/' + file_name + '.datasource'
     with open(file_path, 'w') as f:
         f.write(json.dumps(datasource_setting))
-        print "datasource:{0} is saved to {1}".format(file_name, file_path)
+        print ("datasource:{0} is saved to {1}".format(file_name, file_path))
 
 
 def get_all_datasources_and_save():
     content_of_datasources = search_datasource()
     datasources = json.loads(content_of_datasources)
-    print "There are {0} datasources:".format(len(datasources))
+    print ("There are {0} datasources:".format(len(datasources)))
     for datasource in datasources:
         # print datasource['name']
         save_datasource(datasource['name'], datasource)
--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 This is the grafana_backup main
 """
 __author__ = 'Mihai IDU'
+__version__ = '0.0.1'
 import argparse
 import time
 import datetime
 #from custom_classes import saveDashboards, saveDatasources
-import os, errno
-
+import os, errnoss
 def CreateNestedDirectors(path):
     if not os.path.exists(path):
         try:
@@ -15,6 +15,12 @@ def CreateNestedDirectors(path):
         except OSError as e:
             if e.errno != errno.EEXIST:
                 raise
+# function to trigger a period action.
+def periodic(scheduler, interval, action, actionargs=()):
+    scheduler.enter(interval, 1, periodic,
+                    (scheduler, interval, action, actionargs))
+    action(*actionargs)
+
 
 if __name__ == '__main__':
     # defining the global variable to be accessible to the upper
@@ -30,6 +36,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Process design for Back-up the Grafana dashboards and datasources - Mihai IDU 2021")
     parser.add_argument("-p", "--path", type=str, help="Mention the path where the grafana_backup would manage the backups")
     parser.add_argument("-k", "--api-key", type=str, help="")
+    parser.add_argument("-v", "--version", action='version', version='%(prog)s ' + __version__)
     args = parser.parse_args()
     backup_path = args.path
     GRAFANA_TOKEN = args.api_key
